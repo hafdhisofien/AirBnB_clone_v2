71ffa0e75fa9995c41118d365476630e1edf87a4
hafdhisofien <1114@holbertonschool.com>
2020-04-21 20:39:42 +0000

    c route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+script that starts a Flask web application
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def Hello_HBNB():
+    """
+    returns a string
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    """
+    returns a string
+    """
+    return 'HBNB'
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """
+    display C  followed by the value of the text variable
+    (replace underscore _ symbols with a space )
+    """
+    return 'C ' + text.replace('_', ' ')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
